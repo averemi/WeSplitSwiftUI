deb0600bf5aae4197948227b11be6c879b6ae2ae
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-09-03 15:48:51 +0200

    Added text field wih two-way binding vars.

--- WeSplitSwifUI/ContentView.swift
+++ WeSplitSwifUI/ContentView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    /// Show the value of this property here
+    @State private var tapCount = 0
+    /// Two-way binding: show the value of this property here, but write any changes back to this property - whatever is in the text is also in the name of the property
+    @State private var name = ""
+
+    /// Our views are the function of their state - everything the user can see is just the visible representation of the structs and properties in our code
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Text("Hello world!")
+                    Button ("Tap Count: \(tapCount)") {
+                        tapCount += 1
+                    }
+                    TextField("Enter your name", text: $name)
+                    Text("Your name is \(name)")
+                    ForEach(0 ..< 100) {
+                        Text("Row \($0)")
+                    }
                 }
             }
             .navigationTitle("SwiftUI")
